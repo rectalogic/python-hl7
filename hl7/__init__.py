7374e939673d99163f8dd5b1d56d36903b236aa1
Kevin Gill <kevin.gill@openapp.ie>
2012-03-18 20:49:08 +0000

    Created a Repetition and Component container class. These are just for informational use. The functionality is defined in the base class.

--- hl7/__init__.py
+++ hl7/__init__.py
@@ -276,6 +276,17 @@ class Field(Container):
     """
 
 
+class Repetition(Container):
+    """Fourth level of an HL7 message. A field can repeat.
+    """
+
+
+class Component(Container):
+    """Fifth level of an HL7 message. A component is a composite datatypes.
+    Contains sub-components
+    """
+
+
 def create_parse_plan(strmsg):
     """Creates a plan on how to parse the HL7 message according to
     the details stored within the message.
@@ -306,7 +317,7 @@ def create_parse_plan(strmsg):
         separators.append('&')       # sub-component separator
 
     ## The ordered list of containers to create
-    containers = [Message, Segment, Field, Field, Field]
+    containers = [Message, Segment, Field, Repetition, Component]
     return _ParsePlan(separators, containers)
 
 
